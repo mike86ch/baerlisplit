40aace4c30007fe39033e268dc7c96378d0f6192
mike86ch <46433672+mike86ch@users.noreply.github.com>
2026-08-27 19:19:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,37 @@
-const CACHE_NAME = "baerlisplit-v18";
+const CACHE_NAME = "baerlisplit-v19";
 
 const urlsToCache = [
     "./",
     "./index.html",
     "./style.css",
     "./app.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
 
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
     );
 });
 
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
         caches.match(event.request)
-            .then(response => {
-
-                return response ||
-                    fetch(event.request);
-
-            })
-
+            .then(response => response || fetch(event.request))
     );
 });
